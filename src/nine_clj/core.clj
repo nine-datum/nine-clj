a1b5a75f9bd90d6b7b4b89410fd4fa1e05165d1b
taimuraz <lulookgamedev@gmail.com>
2025-07-09 15:42:33 +0300

    decoupling from lwjgl

--- src/nine_clj/core.clj
+++ src/nine_clj/core.clj
@@ -54,52 +54,80 @@
 (def state (atom {}))
 (def resources (atom {}))
 
-(defn windowLoop [win id dev res-atom]
+(defn window-loop [dev res-atom focused? on-close w h]
+  (cond
+    (or (nil? focused?) (focused?))
+    (do
+      (update-status proc-refresh-status)
+      (graph/reset-matrix-stack)
+      (graph/reset-camera)
+      (graph/reset-light)
+      (graph/reset-projection)
+      (reset! window-width w)
+      (reset! window-height h)
+      (prof/reset)
+      (prof/profile :main-loop (swap! state (partial (@state :loop) dev @res-atom)))
+    )
+  )
+  ((dev :mouse) :update)
+  (-> dev :keyboard input/keyboard-update)
+  (when (and on-close (nil? @state)) (on-close))
+)
+
+(defn window-loop-action [dev res-atom focused? on-close]
   (proxy [WindowLoopAction] []
     (call [w h]
-      (cond
-        (= (. GLFW GLFW_TRUE) (. GLFW glfwGetWindowAttrib id GLFW/GLFW_FOCUSED))
-        (do
-          (update-status proc-refresh-status)
-          (graph/reset-matrix-stack)
-          (graph/reset-camera)
-          (graph/reset-light)
-          (graph/reset-projection)
-          (reset! window-width w)
-          (reset! window-height h)
-          (prof/reset)
-          (prof/profile :main-loop (swap! state (partial (@state :loop) dev @res-atom)))
-        )
-      )
-      ((dev :mouse) :update)
-      (-> dev :keyboard input/keyboard-update)
-      (cond (nil? @state) (.close win))
+      (window-loop dev res-atom focused? on-close w h)
     )
   )
 )
 
-(defn window-start [win setup]
+(defn decode-storage [stream]
+  (let [
+      decoder (nine.io.encoding.ResourcesDecoder.)
+    ]
+    (-> "res.txt" nine.io.FileStorageResource. nine.io.encoding.FileDecoder. (.decode decoder nil))
+    (.storage decoder)
+  )
+)
+
+(defn window-start [setup gl storage keyboard mouse get-width get-height get-time]
+  (let [
+      dev {
+        :storage storage
+        :gl gl
+        :keyboard keyboard
+        :mouse mouse
+        :get-time get-time
+        :width get-width
+        :height get-height
+      }
+    ]
+    (reset! state (menu/loading-menu-setup dev resources setup))
+    dev
+  )
+)
+
+(defn window-start-action [win setup]
   (proxy [WindowStartAction] []
     (start [id]
       (let [
-          decoder (nine.io.encoding.ResourcesDecoder.)
-          _ (-> "res.txt" nine.io.FileStorageResource. nine.io.encoding.FileDecoder. (.decode decoder nil))
-          dev {
-            :storage (.storage decoder)
-            :gl (graph/new-gl)
-            :keyboard (input/keyboard id)
-            :mouse (input/mouse id proc-refresh-status)
-            :get-time get-time
-            :width width
-            :height height
-          }
-          res-atom resources
+          storage (-> "res.txt" java.io.File. java.io.FileInputStream. decode-storage)
+          gl (graph/new-gl)
+          keyboard (input/keyboard id)
+          mouse (input/mouse id proc-refresh-status)
+          get-time get-time
+          width width
+          height height
+          focused? (fn [] (= (. GLFW GLFW_TRUE) (. GLFW glfwGetWindowAttrib id GLFW/GLFW_FOCUSED)))
+          on-close (fn [] (.close win))
         ]
-        ((dev :mouse) :update)
-        (-> dev :keyboard input/keyboard-update)
-        (reset! state (menu/loading-menu-setup dev res-atom setup))
-        ;(org.lwjgl.glfw.GLFW/glfwSwapInterval 0) ; fps unlocker
-        (windowLoop win id dev res-atom)
+        (mouse :update)
+        (input/keyboard-update keyboard)
+        (->
+          (window-start setup gl storage keyboard mouse width height get-time)
+          (window-loop-action resources focused? on-close)
+        )
       )
     )
   )
@@ -108,7 +136,7 @@
 (defn window [w h setup params]
   (let [win (LWJGL_Window.)]
     (.run win w h
-      (window-start win setup)
+      (window-start-action win setup)
     )
   )
 )
